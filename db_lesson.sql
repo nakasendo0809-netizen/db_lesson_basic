4237c87506433aee4b7241516a48df602f0c624d
HAZUKI HOLSTMARK <251836855+nakasendo0809-netizen@users.noreply.github.com>
2026-01-16 16:25:17 +0900

    Q5.年齢の降順で男性の名前と年齢を取得

--- db_lesson.sql
+++ db_lesson.sql
@@ -18,3 +18,5 @@ UPDATE people SET department_id=2 WHERE person_id=3;
 UPDATE people SET department_id=3 WHERE person_id=4;
 UPDATE people SET department_id=4 WHERE person_id=5;
 UPDATE people SET department_id=5 WHERE person_id=6; -- Q4.peopleテーブルの空欄department_idにレコードを挿入
+
+SELECT name,age FROM people WHERE gender=1 ORDER BY age DESC; -- Q5.年齢の降順で男性の名前と年齢を取得
